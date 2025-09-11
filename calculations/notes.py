d3ae9a2736a61b2a20f8ca6cbdd11161944f491b
Nils Edvardsson <nils@nilsjc.com>
2025-09-11 10:47:50 +0200

    init git repo with some data

---
+++ calculations/notes.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = 2047
+r = (4095 / A) ** (1 / 126)
+array = [round(A * (r ** i)) for i in range(127)]
+
+with open("output.txt", "w") as file:
+    file.write(",".join(map(str, array)))
+
+plt.plot(array)
+plt.show()
